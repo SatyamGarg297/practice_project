975c2df9938109d04d2a693245fb9c6c58dfa3c2
SatyamGarg297 <satyamgarg1672001@gmail.com>
2025-09-04 00:14:03 +0530

    added authentication using passport with local strategy and also added Authorization

--- init/index.js
+++ init/index.js
@@ -18,6 +18,7 @@ async function main() {
 
 const initDB = async () => {
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({ ...obj, owner: "68b73d8fb47fcdd9a2f24506" }));
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
     
